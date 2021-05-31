6e2bce50b1dbfe3b9cbca36daf3e10528af79f90
Rafey786 <68226392+Rafey786@users.noreply.github.com>
2021-05-31 11:47:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-// import Mern from './Mern';
-import {BrowserRouter} from 'react-router-dom'
-// import MyPortfolio from './MyPortfolio';
 import Chat from './chat/Index'
 
 ReactDOM.render(
-  <React.StrictMode>
-  <BrowserRouter>
-    {/* <MyPortfolio /> */}
-    {/* <Mern /> */}
         <Chat />
-    {/* <App /> */}
-    </BrowserRouter>
-  </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals();
